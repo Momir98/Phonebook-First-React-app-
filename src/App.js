d658ba032bf918ed82d55d87894ba812f2585d53
Momir <momirilic97@gmail.com>
2022-08-20 18:53:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+
+import React, {useRef, useState} from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
+
+const [show, setShow] = useState(true)
+const [name, setName] = useState(null)
+const [phone, setPhone] = useState(null)
+const [print, setPrint] = useState(false)
+
+const [isHovering, setIsHovering] = useState(false);
+const handleMouseOver = () => {
+  setIsHovering(true);
+};
+const handleMouseOut = () => {
+  setIsHovering(false);
+};
+
+
+
+function getName(val){
+    setName(val.target.value)
+    setPrint(false)
+}
+function getPhone(val) {
+  setPhone(val.target.value)
+  setPrint(false)
+}
+function removeMe(){
+  document.getElementById('data').remove()
+}
+
+
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <Header></Header>
+    
+      <div id="app-body">
+
+<div id="all-con" className="all-con" >
+
+<div id='data' className='data'  onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+{
+print
+?
+(
+<><h3>First and last name: {name}</h3> <h4>Phone number: {phone}</h4></>
+) : null
+}
+{isHovering && (<button id='dltbtn' onClick={removeMe}></button>)}
+</div>
+
+
+
+
+
+    
+
+{show && <input id='name' placeholder='Enter first and last name' onChange={getName}></input>}
+    {show && <input id='number' placeholder='Enter phone number' onChange={getPhone}></input>}
+    {show && <button id='add-btn' onClick={()=> setPrint(true)}></button>}
+
+ <button id='btn' className='btn' onClick={()=> setShow(!show)}>{show === true? 'Colse' : 'Add'} </button>
+
+</div>
+   
+</div>
+     
     </div>
   );
 }
+
+
+
+
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+
+        return(
+    
+            <header>
+                <div className="all-contacts" id="all-contacts" ></div>
+                
+        
+            </header>
+        )
+
+}
+
+export default Header
